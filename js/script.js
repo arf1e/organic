5a48db8026f7f5f2954ec3619a4342531d12f044
arf1e <anotheruselessbox@gmail.com>
2017-07-23 17:34:56 +0300

    Костыльная логика слайдера цитат

--- js/script.js
+++ js/script.js
@@ -3,24 +3,37 @@ var leftSlide = document.querySelector('.slider-controls .left');
 var rightSlide = document.querySelector('.slider-controls .right');
 var currentSlide = 0;
 var slides = document.querySelectorAll('.quote-block .quote');
-var slideSpace = 240;
+var slideSpace = 237;
 var y = 0;
 
-function slideLeft() {   
-  leftSlide.addEventListener ('click', function () {
-  quoteBlock.style.transform = ('translateY(-' + (y - slideSpace) + 'px)');
-  currentSlide--;
-  y -= slideSpace;
+function slideLeft() {
+  leftSlide.addEventListener('click', function () {
+    if (currentSlide > 0) {
+    quoteBlock.style.transform = ('translateY(-' + (y - slideSpace) + 'px)');
+      y -= slideSpace;
+      currentSlide--;
+      rightSlide.classList.remove('disabled');
+      if (currentSlide === 0) {
+        leftSlide.classList.add('disabled');
+      }
+  }
   });
 }
 
 function slideRight() {
-    rightSlide.addEventListener ('click', function () {
-    quoteBlock.style.transform = ('translateY(-' + (y + slideSpace) + 'px)');
-    currentSlide++;
-    y += slideSpace;
+    rightSlide.addEventListener('click', function () {
+      if (currentSlide < slides.length - 1) {
+      quoteBlock.style.transform = ('translateY(-' + (y + slideSpace) + 'px)');
+        y += slideSpace;
+        currentSlide++;
+        leftSlide.classList.remove('disabled');
+        if (currentSlide + 1 === slides.length) {
+          rightSlide.classList.add('disabled');
+        }
+    }
   });
 }
+
 
 slideLeft();
 slideRight();
